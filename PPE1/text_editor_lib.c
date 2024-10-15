daf073cd564da159d9fc7615d2c6e9a9f34c1ebd
ebs233@nau.edu <ebs233@nau.edu>
2024-10-15 00:01:21 -0700

    Finished PPE1

--- PPE1/text_editor_lib.c
+++ PPE1/text_editor_lib.c
@@ -53,24 +53,34 @@ void initEditor(TextEditor* editor)
  */
 void addText(TextEditor* editor, const char* text)
 {
-	// Get the size of the passed string
-	int textSize = sizeof(text)/sizeof(text[0]);
-	
-	// Iterate through the length of the string
-	for (int i = 0; i < textSize; i++)
-	{
-		// Create a new node for the current character
-		Node* newChar = createNode(text[i]);
-		Node* behindCursor = editor->cursor->prev
-		//Link the cursor's prev to the address of newChar
-		behindCursor = newChar;
-		
-		behindCursor->next = newChar;
-		//Link newChar to 
-		newChar->prev = behindCursor;
-		newChar->next = editor->cursor;
-	
-	}
+    // Get the size of the passed string
+    int textSize = strlen(text);
+    
+    // Iterate through the length of the string
+    for(int i = 0; i < textSize; i++)
+    {
+        // Create a new node for the current character
+        Node* newChar = createNode(text[i]);
+        
+        
+        // Store the address of the node behind the cursor
+        Node* behindCursor = editor->cursor->prev;
+        
+        // Set previous cursor's next value to add of newChar (if prev exists)
+        if (behindCursor != NULL)
+        {
+            
+            //Link the next value
+            behindCursor->next = newChar;
+        }
+        // Set newChar's prev value to address of behindCursor
+        newChar->prev = behindCursor;
+        
+        // Set newChar's next to the cursor
+        newChar->next = editor->cursor;
+        // Set cursor's prev value to newChar
+        editor->cursor->prev = newChar;
+    }
 }
 
 
@@ -84,11 +94,26 @@ void addText(TextEditor* editor, const char* text)
  */
 void deleteText(TextEditor* editor, int k)
 {
-	for (int i = 0; i < k; i++)
-	{
-		Node* deleteMe = editor->prev;
-		
-	}
+    //Iterate through how many chars the user wants to delete (k)
+    for (int i = 0; i < k; i++)
+    {
+        // Get the address of the node to delete
+        Node* deleteMe = editor->cursor->prev;
+        
+        if(deleteMe->prev != NULL)
+        {
+            // Have the node before deleteMe change its next to point
+            //to the cursor now instead
+            deleteMe->prev->next = editor->cursor;
+            
+            // Have the cursor's previous value point to the node
+            //before deleteMe
+            editor->cursor->prev = deleteMe->prev;
+            
+            //Free deleteMe so that we aren't using up unnecessary memory
+            free(deleteMe);
+        }
+    }
 }
 
 
@@ -100,7 +125,23 @@ void deleteText(TextEditor* editor, int k)
  * Return: none
  * Dependencies: none
  */
+void cursorLeft(TextEditor* editor, int k)
+{
+    // Iterate through how many nodes the user wants to traverse
+    //to the left with
+    for (int i = 0; i < k; i++)
+    {
+        // Store the address of the current node to the left of the cursor
+        Node* prevNode = editor->cursor->prev;
 
+        // Check if the previous node has reached the start
+        if (prevNode != editor->head)
+        {
+            // If not, then traverse 1 node to the left
+            editor->cursor = prevNode;
+        }
+    }    
+}
 
 
 /**
@@ -111,7 +152,23 @@ void deleteText(TextEditor* editor, int k)
  * Return: none
  * Dependencies: none
  */
-
+void cursorRight(TextEditor* editor, int k)
+{
+    // Iterate through how many nodes the user wants to traverse
+    //to the right with
+    for (int i = 0; i < k; i++)
+    {
+        //Store the address of the current node to the right of the cursor
+        Node* nextNode = editor->cursor->next;
+        
+        // Check if next node has reached the end of the list
+        if(nextNode != NULL)
+        {
+            //If not, traverse 1 node to the right
+            editor->cursor = nextNode;
+        }
+    }
+}
 
 
 /**
@@ -121,7 +178,42 @@ void deleteText(TextEditor* editor, int k)
  * Return: none
  * Dependencies: stdio.h
  */
-
+void displayText(const TextEditor* editor)
+{
+    // Get the head node
+    Node* start = editor->head;
+    
+    // Initialize current node to traverse the deque,
+    //starting at the left
+    Node* current = start;
+    
+    // Traverse the whole deque from start to end
+    while(current->next != NULL)
+    {
+        // Check if the current held value is the cursor
+        if(current == editor->cursor)
+        {
+            // If so, print a cursor indicator before printing our char
+            printf("|");
+        }
+        
+        //Print the character of the current held pointer
+        printf("%c", current->data);
+        
+        // Traverse to next item in the deque
+        current = current->next;
+    }
+    
+    // If the cursor is at the end, print it
+    if(editor->cursor == editor->tail)
+    {
+        // Print cursor indicator
+        printf("|");
+    }
+    
+    //Print a newline char
+    printf("\n");
+}
 
 
 /**
@@ -132,4 +224,103 @@ void deleteText(TextEditor* editor, int k)
  * Return: none
  * Dependencies: string.h, stdio.h
  */
-
+void findAndHighlight(TextEditor* editor, const char* text)
+{
+    // Initialize start pointer for head
+    Node* start = editor->head;
+    // Initialize current pointer to traverse the deque from left to right
+    Node* current = start;
+    // Initialize a helper var to hold the size of the passed text to
+    //search for
+    int textSize = strlen(text);
+    // Initialize an array to hold the pointers of our matched search
+    Node* textArray[textSize];
+    
+    // Initialize an index to iterate through textArray
+    int textIndex = 0;
+    // Initialize a variable to indicate if we've successfully found abort
+    //match in the search
+    bool foundText = false;
+    
+    // If the user didn't enter a search...
+    if(textSize == 0)
+    {
+        // Print this message to the user
+        printf("Invalid search text. Please provide a text to search for.");
+    }
+    // If the user DID enter a valid search
+    else
+    {
+        // Traverse the deque while we haven't found a match
+        while(current != editor->tail && !foundText)
+        {
+            // If the current value matches a sequential part of our search
+            if(current->data == text[textIndex] )
+            {
+                // Assign it to our array of pointers
+                textArray[textIndex] = current;
+                // Increment the index
+                textIndex++;
+                
+                // If our textIndex matches the size of our text, then we've
+                //found a full match
+                if(textIndex == textSize)
+                {
+                    // Update our bool to indicate this
+                    foundText = true;
+                }
+            }
+            else
+            {
+                // If at any point in our sequential matching process,
+                //we find a mismatched char, reset the textIndex.
+                textIndex = 0;
+            }
+            
+            // Traverse to the next address in the deque
+            current = current->next;
+        }
+    
+        // If we haven't found our match...
+        if(!foundText)
+        {
+            // Print a message to the user.
+            printf("String is not found in the text. Please try again.");
+        }
+        // If we HAVE found our match
+        else
+        {
+            // Re-init current pointer
+            current = start;
+            // Reset textIndex int
+            textIndex = 0;
+            // Iterate through the deque again to start the
+            //printing process
+            while(current != editor->tail)
+            {
+                // If we've found the start of our match...
+                if(current == textArray[0])
+                {
+                    // Start our highlight in the output
+                    printf("[");
+                }
+                
+                // Print the current char to the user.
+                printf("%c", current->data);
+                
+                // If we've found the end of our match...
+                if(current == textArray[textSize - 1])
+                {
+                    // End our highlight in the output
+                    printf("]");
+                }
+                // Traverse to the next pointer in the deque
+                current = current->next;
+            }
+        }
+    }
+    
+    // Print a newline char here so I don't have to type it in all of
+    //the printfs
+    printf("\n");
+}
